cd576825103abf3ca1688d0fd8aa9ed5b1bc7b19
Patrizio <patrick@rekeeb.com>
2011-08-22 20:56:00 +0200

    Added Annotate gem

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,18 @@
 class Employee < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :integer         not null, primary key
+#  firstname  :string(255)
+#  lastname   :string(255)
+#  title      :string(255)
+#  department :string(255)
+#  extension  :integer
+#  mobile     :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- test/unit/employee_test.rb
+++ test/unit/employee_test.rb
@@ -6,3 +6,19 @@ class EmployeeTest < ActiveSupport::TestCase
     assert true
   end
 end
+
+# == Schema Information
+#
+# Table name: employees
+#
+#  id         :integer         not null, primary key
+#  firstname  :string(255)
+#  lastname   :string(255)
+#  title      :string(255)
+#  department :string(255)
+#  extension  :integer
+#  mobile     :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
