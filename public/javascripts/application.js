e41f11b2f8e6a83870a2b6c426dcbb15a1afa8c1
Patrizio <patrick@rekeeb.com>
2011-09-11 17:23:03 +0200

    Using fancybox to show iframed page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+	$("a.employee").live("click",function(ev){
+    	ev.preventDefault();
+    	$.fancybox({
+        	type: "iframe",
+			href: this
+    	})
+	});
+});
+
 $(function() {
 	$("#employees_search input").keyup( function() {
 		$.get($("#employees_search").attr("action"), $("#employees_search").serialize(), null, "script");
