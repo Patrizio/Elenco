441b870aa2a626c40922bb001ea3e8e8aadac0f6
Patrizio <patrick@rekeeb.com>
2011-10-09 20:23:08 +0200

    Enable asset pipeline and placed fancybox in vendor-assets

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,33 @@
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.fancybox-1.3.1
+//= require_tree .
+
+$(document).ready(function () {
+	$("a.employee-details").live("click",function(ev){
+    	ev.preventDefault();
+    	$.fancybox({
+        	type: "iframe",
+			href: this,
+			'transitionIn'	:	'elastic',
+			'transitionOut'	:	'elastic',
+			'speedIn'		:	150, 
+			'speedOut'		:	100, 
+			'overlayShow'	:	true,
+			'height'		: 	450
+    	})
+	});
+});
+
+$(function() {
+	$("#employees_search input").keyup( function() {
+		$.get($("#employees_search").attr("action"), $("#employees_search").serialize(), null, "script");
+		return false;
+	});
+});
--- config/application.rb
+++ config/application.rb
@@ -31,13 +31,16 @@ module Elenco
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :nl
 
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
     # Configure the default encoding used in templates for Ruby 1.9.
-    #config.encoding = "utf-8"
+    config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
