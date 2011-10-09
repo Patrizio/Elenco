d9992fe9edabb68cdd684ebc2169601746dfa96e
Patrizio <patrick@rekeeb.com>
2011-10-09 15:00:36 +0200

    Wrote comments in the Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -18,15 +18,20 @@
 #
 
 class Employee < ActiveRecord::Base
+  
+  # Validate the following fields and their specific requirements 
   validates_presence_of :firstname, :lastname, :title, :department, :extension
   validates_numericality_of :extension
   validates_length_of :extension, :is => 4
   validates_uniqueness_of :extension,:mobile, :allow_blank => true
   
+  # Each employee has a profile picture
   has_attached_file :avatar, :styles => { :medium => "300x300>", :small => "200x200", :thumb => "100x100>" } 
   
+  # The default way items are ordered/presented
   default_scope :order => 'extension'
   
+  # Live search is performed on the columns specified here
   scope :search_significant_fields, lambda { |q|
     (q ? where(["firstname LIKE ? or lastname LIKE ? or department LIKE ?", '%'+ q + '%', '%'+ q + '%', '%'+ q + '%'])  : {})
   }  
