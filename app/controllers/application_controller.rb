1d5b219d1d898f99cc978b1867d0204fa9b887e2
Patrizio <patrick@rekeeb.com>
2011-10-10 23:18:41 +0200

    Added basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  # basic authentication
+  before_filter :authentication_check
+    
   # pass the locale with every request
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
+  end
+  
+  private
+    def authentication_check
+      authenticate_or_request_with_http_basic do |user, password|
+        user == "elenco" && password == "stayokay"
+    end
   end
   
 end
