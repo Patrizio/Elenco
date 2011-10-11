3248bbdde9fe4462d8b1a7c0d2cc74072f2f8e4c
Patrizio <patrick@rekeeb.com>
2011-10-11 23:21:10 +0200

    Commented X-sendfile as seen in other project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Elenco::Application.configure do
   config.action_controller.perform_caching = true
 
   # Specifies the header that your server uses for sending files
-  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile"
   
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
