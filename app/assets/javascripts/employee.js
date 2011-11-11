b0f7c32126335fee09765092269eb6c372c66918
Patrizio <patrick@rekeeb.com>
2011-11-11 21:39:33 +0100

    combined function on search field

--- app/assets/javascripts/employee.js
+++ app/assets/javascripts/employee.js
@@ -1,8 +1,6 @@
 // Employee.js
 // Contains logic related to searching/showing/hiding employees based on user 
 
-
-
 $(document).ready(function () {
 	$(".employee").live("click",function(ev){
     	ev.preventDefault();
@@ -17,21 +15,20 @@ $(document).ready(function () {
 			'height'		: 	600
     	})
 	});
-	// see if the search field has been cleared (click X)
-	$('#search').click(function(e) {
-		var searchString = this.value;
-		if(searchString == "" ) {
-			// search field is empty
-			// show all employees
-			$.get($("#employees_search").attr("action"), $("#employees_search").serialize(), null, "script");
-			return false;		
-		}
-	});
-});
 
-$(function() {
-	$("#employees_search input").keyup( function() {
-		$.get($("#employees_search").attr("action"), $("#employees_search").serialize(), null, "script");
-		return false;
-	});
+	$("#search").bind({
+		keyup: function () {
+				$.get($("#employees_search").attr("action"), $("#employees_search").serialize(), null, "script");
+				return false;
+		},
+		click: function () {
+				var searchString = this.value;
+				if(searchString == "" ) {
+					// search field is empty
+					// show all employees
+					$.get($("#employees_search").attr("action"), $("#employees_search").serialize(), null, "script");
+					return false;		
+				}
+		}
+	})
 });
