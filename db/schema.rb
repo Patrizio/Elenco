3a41ae4dc63db86917dedfdb09dce1e3badd292b
Patrizio <patrick@rekeeb.com>
2011-11-13 21:06:25 +0100

    wrote migration to include e-mail|

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111011174857) do
+ActiveRecord::Schema.define(:version => 20111113200354) do
 
   create_table "employees", :force => true do |t|
     t.string   "firstname"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20111011174857) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "email"
   end
 
   create_table "taggings", :force => true do |t|
