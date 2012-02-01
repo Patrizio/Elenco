2d66f0d38b3bd5f6c7074920a72cd3dfdfaff758
Patrizio <patrick@rekeeb.com>
2012-02-01 23:17:26 +0100

    corrected fabrication of extension of 4 integers

--- spec/fabricators/employee_fabricator.rb
+++ spec/fabricators/employee_fabricator.rb
@@ -1,7 +1,7 @@
 Fabricator(:employee) do
   firstname { Faker::Name.first_name }
   lastname { Faker::Name.last_name } 
-  extension { sequence(:extension, 1) }
+  extension { sequence(:extension, 1111) }
   department 'Ruby coders'
   title 'Dev'
   email { Faker::Internet.email }
