0a31bccaf629a4e1f53bae40d97ed26be87a53c6
Patrizio <patrick@rekeeb.com>
2012-02-18 14:57:26 +0100

    Added Company faker

---
+++ spec/fabricators/company_fabricator.rb
@@ -0,0 +1,8 @@
+Fabricator(:company) do
+  name { Faker::Company.name }
+  phone { Faker::PhoneNumber.phone_number } 
+  address { Faker::Address.street_address }
+  postalcode { Faker::Address.postcode }
+  city { Faker::Address.city }
+  country { Faker::Address.country }
+end
