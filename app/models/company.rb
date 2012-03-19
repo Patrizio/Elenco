173edbaff6cd9bc3db8f0748febd7f12969ebfed
Patrizio <patrick@rekeeb.com>
2012-03-19 20:14:31 +0100

    Latest changes from last month

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,7 @@
 class Company < ActiveRecord::Base
   has_many :employees, :dependent => :destroy
 
+  # Validate the following fields and their specific requirements 
+  # validates_presence_of :firstname, :lastname, :title, :department, :extension, :email
+
 end
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it { should_not be_valid }
+  
+  it { should have(1).error_on(:name) }
+  it { should have(1).error_on(:phone) }
+  it { should have(1).error_on(:postalcode) }
+  it { should have(1).error_on(:address) }
+  it { should have(1).error_on(:city) }
+  it { should have(1).error_on(:country) }
+  
+  context "after fabrication" do
+    
+    subject { Fabricate(:company) }
+    
+    it { should be_valid }
+    
+  end
+  
 end
