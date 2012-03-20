8162f8962cc11215ac9a35113fb00021cb9bbdd4
Patrizio <patrick@rekeeb.com>
2012-03-20 10:22:37 +0100

    Added subdomain field to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120128144655) do
+ActiveRecord::Schema.define(:version => 20120320091633) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(:version => 20120128144655) do
     t.string   "country"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "subdomain"
   end
 
   create_table "employees", :force => true do |t|
