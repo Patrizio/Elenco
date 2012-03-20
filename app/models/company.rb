fd69058bae2b5ab8d68fe36836c2489bcb3fcffe
Patrizio <patrick@rekeeb.com>
2012-03-20 08:56:27 +0100

    Finished basic company spec

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,6 @@
 class Company < ActiveRecord::Base
   has_many :employees, :dependent => :destroy
 
-  # Validate the following fields and their specific requirements 
-  # validates_presence_of :firstname, :lastname, :title, :department, :extension, :email
+  validates_presence_of :name, :phone, :address, :postalcode, :city, :country
 
 end
