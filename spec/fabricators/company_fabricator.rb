e598fec70513037b1b6638ef838fcc9cd44f6ddb
Patrizio <patrick@rekeeb.com>
2012-03-20 10:44:19 +0100

    added subdomain faker

--- spec/fabricators/company_fabricator.rb
+++ spec/fabricators/company_fabricator.rb
@@ -1,5 +1,6 @@
 Fabricator(:company) do
   name { Faker::Company.name }
+  subdomain { Faker::Company.name } 
   phone { Faker::PhoneNumber.phone_number } 
   address { Faker::Address.street_address }
   postalcode { Faker::Address.postcode }
