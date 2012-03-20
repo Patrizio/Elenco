9a16ef7810f26ca738900f70159e4654827d0738
Patrizio <patrick@rekeeb.com>
2012-03-20 10:29:37 +0100

    Added validation for subdomain

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,6 @@ class Company < ActiveRecord::Base
   has_many :employees, :dependent => :destroy
 
   validates_presence_of :name, :phone, :address, :postalcode, :city, :country
-
+  validates :subdomain, :presence => true, :uniqueness => true
+  
 end
