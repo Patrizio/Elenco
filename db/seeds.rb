1eae933b69846c218c7b672b717d7aa131c16592
Patrick Beeker <patrick@rekeeb.com>
2012-11-05 14:29:58 +0100

    added 2 companies to seed in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Company.create(name: 'Stayokay', subdomain: 'stayokay', phone: '020-12345678', address: 'Timorplein 21a', postalcode: '1099CC', city: 'Amsterdam', country: 'The Netherlands')
+Company.create(name: 'Mangrove', subdomain: 'mangrove', phone: '010-12345678', address: 'Schiekade 12', postalcode: '1299CC', city: 'Rotterdam', country: 'The Netherlands')
