95830766f16da5aabb519e2f6ab754f0cf8a822d
Patrick Beeker <patrick@rekeeb.com>
2012-11-05 16:19:20 +0100

    added status message while seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+puts "START SEEDING DB"
+puts "Seeding companies..."
 Company.create(name: 'Stayokay', subdomain: 'stayokay', phone: '020-12345678', address: 'Timorplein 21a', postalcode: '1099CC', city: 'Amsterdam', country: 'The Netherlands')
 Company.create(name: 'Mangrove', subdomain: 'mangrove', phone: '010-12345678', address: 'Schiekade 12', postalcode: '1299CC', city: 'Rotterdam', country: 'The Netherlands')
+puts "DONE...SEEDING DB"
