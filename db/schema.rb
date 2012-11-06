0c7be15bc9a31b05a99aabc3feb8bed6c079be17
Patrick Beeker <patrick@rekeeb.com>
2012-11-06 16:11:12 +0100

    Added slug field to User datamodel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121106142902) do
+ActiveRecord::Schema.define(:version => 20121106150813) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -76,10 +76,12 @@ ActiveRecord::Schema.define(:version => 20121106142902) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "slug"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["name"], :name => "index_users_on_name", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["slug"], :name => "index_users_on_slug", :unique => true
 
 end
