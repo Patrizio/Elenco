79eaad656eaeaa5615ecef89a81c3ddf0c810a47
Patrick Beeker <patrick@rekeeb.com>
2012-11-06 16:06:20 +0100

    Included 2 users as db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,7 @@ puts "START SEEDING DB"
 puts "Seeding companies..."
 Company.create(name: 'Stayokay', subdomain: 'stayokay', phone: '020-12345678', address: 'Timorplein 21a', postalcode: '1099CC', city: 'Amsterdam', country: 'The Netherlands')
 Company.create(name: 'Mangrove', subdomain: 'mangrove', phone: '010-12345678', address: 'Schiekade 12', postalcode: '1299CC', city: 'Rotterdam', country: 'The Netherlands')
+puts "Seeding users..."
+user1 = User.create! :name => 'First User', :email => 'user@test.com', :password => 'please', :password_confirmation => 'please'
+user2 = User.create! :name => 'Other User', :email => 'otheruser@test.com', :password => 'please', :password_confirmation => 'please'
 puts "DONE...SEEDING DB"
