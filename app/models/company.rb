9db3b3f47fe5f0f4788413828f050edf9f716bde
Patrick Beeker <patrick@rekeeb.com>
2012-11-06 15:28:09 +0100

    Use friendly_id in the Company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,10 @@
 class Company < ActiveRecord::Base
+  extend FriendlyId
+
   has_many :employees, :dependent => :destroy
 
   validates_presence_of :name, :phone, :address, :postalcode, :city, :country
   validates :subdomain, :presence => true, :uniqueness => true
-  
+  friendly_id :name, use: :slugged
+
 end
