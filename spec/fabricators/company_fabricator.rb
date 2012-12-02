918079d0d91cf30e8ef32fae84c6ed7f744cccf4
Patrizio <patrick@rekeeb.com>
2012-12-02 13:27:21 +0100

    Added fax, po-box to fabricator

--- spec/fabricators/company_fabricator.rb
+++ spec/fabricators/company_fabricator.rb
@@ -1,9 +1,11 @@
 Fabricator(:company) do
   name { Faker::Company.name }
   subdomain { Faker::Company.name } 
-  phone { Faker::PhoneNumber.phone_number } 
+  phone { Faker::PhoneNumber.phone_number }
+  fax { Faker::PhoneNumber.fax_number } 
   address { Faker::Address.street_address }
   postalcode { Faker::Address.postcode }
+  po_box { sequence(:po_box, 11111 ) }
   city { Faker::Address.city }
   country { Faker::Address.country }
 end
