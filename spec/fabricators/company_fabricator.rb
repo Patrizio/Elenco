2f5654c1e93601f79143da89eb0985c0abcfc957
Patrizio <patrick@rekeeb.com>
2012-12-02 13:32:41 +0100

    Corrected wrong fax variables

--- spec/fabricators/company_fabricator.rb
+++ spec/fabricators/company_fabricator.rb
@@ -2,7 +2,7 @@ Fabricator(:company) do
   name { Faker::Company.name }
   subdomain { Faker::Company.name } 
   phone { Faker::PhoneNumber.phone_number }
-  fax { Faker::PhoneNumber.fax_number } 
+  fax_number { Faker::PhoneNumber.phone_number } 
   address { Faker::Address.street_address }
   postalcode { Faker::Address.postcode }
   po_box { sequence(:po_box, 11111 ) }
