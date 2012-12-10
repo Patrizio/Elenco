66f074dae8d8482ab86774d8ac27db0ce2a90a31
Patrizio <patrick@rekeeb.com>
2012-12-10 21:57:40 +0100

    Improved regex for valid subdomain names

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,6 +3,6 @@ class Company < ActiveRecord::Base
 
   validates_presence_of :name, :phone, :address, :postalcode, :city, :country
   validates :subdomain, :presence => true, :uniqueness => true
-  validates_format_of :subdomain, :with => /^[\w\d\s]+-?[\w\d\s]*$/ # Characters only, and at most one dash somewhere in the middle
+  validates_format_of :subdomain, :with => /^[a-z\d]+([-_][a-z\d]+)*$/i # Characters only, and at most one dash somewhere in the middle
   
 end
