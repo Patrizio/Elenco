31dc7006a9569ff9596db20ef0d91cacdf4c1818
Patrizio <patrick@rekeeb.com>
2012-12-15 23:16:03 +0100

    edit the basic controller code

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,72 @@
 class DepartmentsController < ApplicationController
+  # GET /departments
+  # GET /departments.json
+  def index
+    @departments = Department.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @departments }
+    end
+  end
+
+  def show
+    @department = Department.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @department }
+    end
+  end
+
+  def new
+    @department = Department.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @department }
+    end
+  end
+
+  def edit
+    @department = Department.find(params[:id])
+  end
+
+  def create
+    @department = Department.new(params[:department])
+
+    respond_to do |format|
+      if @department.save
+        format.html { redirect_to @department, notice: 'Department was successfully created.' }
+        format.json { render json: @department, status: :created, location: @department }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @department.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @department = Department.find(params[:id])
+
+    respond_to do |format|
+      if @department.update_attributes(params[:department])
+        format.html { redirect_to @department, notice: 'department was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @department.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @department = Department.find(params[:id])
+    @department.destroy
+
+    respond_to do |format|
+      format.html { redirect_to departments_url }
+      format.json { head :ok }
+    end
+  end
 end
