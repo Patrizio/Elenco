e9361884b5b0df520704c3fb153a36c7e3fde058
Patrizio <patrick@rekeeb.com>
2012-12-15 21:26:38 +0100

    Associate Company to departments

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
   has_many :employees, :dependent => :destroy
+  has_many :departments, :dependent => :destroy
 
   validates_presence_of :name, :phone, :address, :postalcode, :city, :country
   validates :subdomain, :presence => true, :uniqueness => true
