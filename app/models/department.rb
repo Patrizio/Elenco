68e099018ef4e41e54de3f93899bba51a1b0e47e
Patrizio <patrick@rekeeb.com>
2012-12-22 17:17:14 +0100

    Make association between employee and department

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,7 @@
 class Department < ActiveRecord::Base
   belongs_to :company
+  has_many :employees
+  
   attr_accessible :email, :extension, :name
   
   validates_presence_of :name, :extension, :email
