d980e18f323ef54038c53d6ffe3017e0f0fecb48
Patrizio <patrick@rekeeb.com>
2013-01-05 22:47:07 +0100

    removed unused GA-tracking code

--- config/application.rb
+++ config/application.rb
@@ -28,11 +28,6 @@ module Elenco
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
-    # Set property for google analytics
-    if Rails.env == "production"
-      config.middleware.use "Rack::GoogleAnalytics", :web_property_id => "UA-26182142-1"
-    end
     
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config/application.rb
