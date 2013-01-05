47833247f7f43a0eb0f473c6d0c944a7a8e9591d
Patrizio <patrick@rekeeb.com>
2013-01-05 21:40:03 +0100

    Basic http protecion in production only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  def authenticate
+  	if Rails.env.production?
+		authenticate_or_request_with_http_basic do |username, password|
+      		username == 'digidex' && password == 'digidex'
+    	end
+    end
+  end
       
   # pass the locale with every request
   def default_url_options(options={})
