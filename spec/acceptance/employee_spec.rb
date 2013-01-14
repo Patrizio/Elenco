a4cced71878993b98c43d40621331cf168818081
Patrizio <patrick@rekeeb.com>
2013-01-14 16:20:12 +0100

    Inspect employee view with capybara

---
+++ spec/acceptance/employee_spec.rb
@@ -0,0 +1,20 @@
+require 'acceptance/acceptance_helper'
+
+feature "Employees" do
+  
+  background :all do
+    @patrick = Fabricate.build(:employee, :firstname => 'Patrick', :lastname => 'Beeker', :email => 'p.beeker@stayokay.com')
+  end
+  
+  background :each do
+    visit '/nl'
+  end
+
+  it "displays the fullname as h1" do
+    rendered.should have_selector 'h1',
+          :text => '@employee.fullname',
+          :count => 1
+  end
+
+  
+end
