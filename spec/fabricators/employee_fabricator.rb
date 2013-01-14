f685840c8ced21074a518a1b72b85d9af971eb4d
Patrizio <patrick@rekeeb.com>
2013-01-14 16:18:25 +0100

    associate generate an employee (with company)

--- spec/fabricators/employee_fabricator.rb
+++ spec/fabricators/employee_fabricator.rb
@@ -1,7 +1,8 @@
 Fabricator(:employee) do
-  firstname { Faker::Name.first_name }
-  lastname { Faker::Name.last_name } 
-  extension { sequence(:extension, 1111) }
-  title 'Dev'
-  email { Faker::Internet.email }
+	company
+  	firstname { Faker::Name.first_name }
+  	lastname { Faker::Name.last_name } 
+  	extension { sequence(:extension, 1111) }
+  	title { Faker::Name.title }
+  	email { Faker::Internet.email }
 end
