29ad5e770270ef3eaf5219133d18403948041a39
Patrizio <patrick@rekeeb.com>
2013-01-15 14:53:47 +0100

    Added companies seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+stayokay = Company.create({ 
+	:name => 'Stayokay', 
+	:subdomain => 'stayokay', 
+	:phone => '020-12345678',
+	:fax_number => '',
+	:address => 'Timorplein 21',
+	:postalcode => '1234 AB',
+	:po_box => '12345',
+	:city => 'Amsterdam',
+	:country => 'the Netherlands'})
+
+saatchi = Company.create({ 
+	:name => 'Saatchi & Saatchi AMS', 
+	:subdomain => 'saatchi', 
+	:phone => '020-12345678',
+	:fax_number => '',
+	:address => 'Danzigerkade 23c',
+	:postalcode => '1234 AB',
+	:po_box => '12345',
+	:city => 'Amsterdam',
+	:country => 'the Netherlands'})
